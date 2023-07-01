ccd89b214e4476ab087ddb2eb0109f8367f4ecf0
Aleksei Zolotov <freopen@freopen.org>
2023-07-01 16:25:51 +0200

    Restart every 60 seconds

--- flake.nix
+++ flake.nix
@@ -13,7 +13,13 @@
         packages.default = channels.nixpkgs.runCommand "freopen_chat_bot" {
           src = ./.;
           __noChroot = true;
-          nativeBuildInputs = with channels.nixpkgs; [ pkg-config protobuf cargo rustc gcc ];
+          nativeBuildInputs = with channels.nixpkgs; [
+            pkg-config
+            protobuf
+            cargo
+            rustc
+            gcc
+          ];
           buildInputs = with channels.nixpkgs; [ openssl ];
         } ''
           export CARGO_HTTP_CAINFO="${channels.nixpkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
@@ -31,36 +37,34 @@
       nixosModules.freopen_chat_bot = { config, pkgs, lib, ... }: {
         options.services.freopen_chat_bot = {
           enable = lib.mkEnableOption "freopen_chat_bot";
-          envFile = lib.mkOption {
-            type = lib.types.str;
+          envFile = lib.mkOption { type = lib.types.str; };
+        };
+        config = let
+          opts = config.services.freopen_chat_bot;
+          pkg = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+        in lib.mkIf opts.enable {
+          users.groups.freopen_chat_bot = { };
+          users.users.freopen_chat_bot = {
+            isSystemUser = true;
+            group = "freopen_chat_bot";
+          };
+          systemd.services.freopen_chat_bot = {
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network-online.target" ];
+            wants = [ "network-online.target" ];
+            startLimitIntervalSec = 0;
+            serviceConfig = {
+              User = "freopen_chat_bot";
+              ExecStart = "${pkg}/bin/chat_bot";
+              EnvironmentFile = opts.envFile;
+              WorkingDirectory = "/var/lib/freopen_chat_bot";
+              StateDirectory = "freopen_chat_bot";
+              StateDirectoryMode = "0700";
+              Restart = "always";
+              RestartSec = 60;
+            };
           };
         };
-        config =
-          let
-            opts = config.services.freopen_chat_bot;
-            pkg = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
-          in
-          lib.mkIf opts.enable {
-            users.groups.freopen_chat_bot = { };
-            users.users.freopen_chat_bot = {
-              isSystemUser = true;
-              group = "freopen_chat_bot";
-            };
-            systemd.services.freopen_chat_bot = {
-              wantedBy = [ "multi-user.target" ];
-              after = [ "network-online.target" ];
-              wants = [ "network-online.target" ];
-              serviceConfig = {
-                User = "freopen_chat_bot";
-                ExecStart = "${pkg}/bin/chat_bot";
-                EnvironmentFile = opts.envFile;
-                WorkingDirectory = "/var/lib/freopen_chat_bot";
-                StateDirectory = "freopen_chat_bot";
-                StateDirectoryMode = "0700";
-                Restart = "always";
-              };
-            };
-          };
       };
     };
 }
